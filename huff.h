c8f0f2c812d6810072f6db542e108f7d9a56b6a4
Tom Pollard <tpollard@purdue.edu>
2011-11-11 15:15:46 -0500

    Added Node auxiliary functions (construct, destruct, and insert)

--- huff.h
+++ huff.h
@@ -31,8 +31,47 @@
         struct binarynode *right;
     } Node;
     
+    /* Function to create a new Node and initialize values to 0 */
+    Node* node_construct() {
+        Node * n = malloc(sizeof(node));
+        n->count = 0;
+        n->character = '\0'
+        n->left = NULL;
+        n->right = NULL;
+        return n;
+    }
     
-
+    /* Function to deallocate memory for node */
+    void node_destruct(Node * n) {
+        free(n);
+    }
+    
+    /* Function to insert a node into a linked list in sorted order */
+    Node* insert_node(Node * head, Node * n) {
+        // If the list is empty, then return the new element
+        if (head == NULL) {
+            return n;
+        }
+        // Go through list until new element's spot is found
+        Node* prev = NULL;
+        Node* cur = head;
+        while ((cur != NULL) && (cur->count < n->count)) {
+            prev = cur;
+            cur = cur->right;
+        }
+        // If new element is the smallest, place it at the front and return it
+        if (cur == head) {
+            n->right = cur;
+            return n;
+        }
+        // Otherwise, place the new element in its position and return the head
+        prev->right = n;
+        n->right = cur;
+        return head;
+    }
+            
+            
+            
 
 
 #endif
