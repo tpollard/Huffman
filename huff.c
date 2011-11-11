3d995587b41e86230fcddff4359558b9055edbf5
Tom Pollard <tpollard@purdue.edu>
2011-11-11 11:15:41 -0500

    Added file input and error checking

--- huff.c
+++ huff.c
@@ -16,10 +16,31 @@
 */
 
    
-// Includes
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int main(int argc, char * argv[])
+{
+    /* Ensure the user entered a filename */
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return -1;
+    }
+    /* Open file as "binary, readonly" */
+    FILE* fhd = fopen(argv[1], "rb");
+        
+    /*Ensure the file can be opened */
+    if (fhd == NULL) {
+        printf("ERROR: %s was not opened correctly.\n", argv[1]);
+        return -1;
+    }
+    
+    /* Close the input file */
+    fclose(fhd);
+    
+    
+    return 0;
+}
+    
 
 
