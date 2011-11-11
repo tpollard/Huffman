16f983e327b710f29cd3a9f4c3558a3ad125b72e
Tom Pollard <tpollard@purdue.edu>
2011-11-11 10:46:46 -0500

    Created huff.c main file

---
+++ huff.c
@@ -0,0 +1,25 @@
+/*  ECE368 Project 2: Huffman Compression and Decompression
+*   Contributors:
+*       Thomas Pollard
+*       Trenton Andres
+*
+*   Created: 11/11/2011
+*
+*   Description:
+*           This is the main program for the Huffman Compression algorithm.
+*       The program takes one (1) command line argument, which is the file to
+*       be compressed. The algorithm uses Huffman coding to compress the input
+*       file, and stores the compressed file as <filename.extension>.huff
+*
+*   Last Modified: 11/11/2011
+*
+*/
+
+   
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
