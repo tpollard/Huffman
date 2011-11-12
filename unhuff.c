0c7007c2ba0546a639e06d9cdb1c66c6529d740f
Tom Pollard <tpollard@purdue.edu>
2011-11-12 16:32:18 -0500

    Created unhuff.c and unhuff.h files

---
+++ unhuff.c
@@ -0,0 +1,38 @@
+/*  ECE368 Project 2: Huffman Compression and Decompression
+*   Contributors:
+*       Thomas Pollard
+*       Trenton Andres
+*
+*   File: unhuff.c
+*
+*   Created: 11/12/2011
+*
+*   Description:
+*           This is the main program for the Huffman Decompression algorithm.
+*       The program takes one (1) command line argument, which is the file to
+*       be decompressed. The algorithm uses Huffman coding to decompress the
+*		input file, and stores the decompressed file as 
+*		<filename.extension>.unhuff
+*
+*   Last Modified: 11/12/2011
+*
+*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "unhuff.h"
+
+// Debug flags for printing intermediate results
+
+
+
+int main(int argc, char * argv[])
+{
+    
+    
+    return 0;
+}
+    
+
+
---
+++ unhuff.h
@@ -0,0 +1,26 @@
+/*  ECE368 Project 2: Huffman Compression and Decompression
+*   Contributors:
+*       Thomas Pollard
+*       Trenton Andres
+*
+*   File: unhuff.h
+*
+*   Created: 11/12/2011
+*
+*   Description:
+*           This is the header file for the Huffman Decompression algorithm.
+*       This header declares the node structure that is used to build the 
+*       binary tree, as well as support functions. This header also includes
+*       other function prototypes
+*
+*   Last Modified: 11/12/2011
+*
+*/
+
+#ifndef __UNHUFFH__
+#define __UNHUFFH__
+
+    
+
+
+#endif
