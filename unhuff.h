8902a85e92440cb51da68867a78052a095ff1649
Tom Pollard <tpollard@purdue.edu>
2011-11-16 23:02:54 -0500

    Added tree() function to create binary tree from header

--- unhuff.h
+++ unhuff.h
@@ -20,17 +20,77 @@
 #ifndef __UNHUFFH__
 #define __UNHUFFH__
 
+#include "huff.h"
+
+    global Node * head;
+    global FILE * fhd;
+    
+    /* This function builds the binary Huffman Coding tree */
 	Node * tree(char bit, char byte, Node * curpos) {
-		/* curpos == NULL when tree is built 
+		/* curpos == NULL when tree is finished being built (initially == head)
 		 * byte is invalid unless bit == 1
 		 */
-		return curpos;
+		 
+		 /* When the tree is complete, curpos will point to NULL, so return */
+		 if (curpos == NULL) {
+		    return NULL;
+		 }
+		 
+		 /* If the bit that was read is a zero, */
+		 if (bit == 0) {
+		    /* Try to make a new child on the left */
+		    if (curpos->left == NULL) {
+		        Node * n = node_construct(0,0);
+		        curpos->left = n;
+		        n->next = curpos;
+		        return n;
+		    }
+		    /* Try to make a new child on the right */
+		    else if (curpos->right == NULL) {
+		        Node * n = node_construct(0,0);
+		        curpos->right = n;
+		        n->next = curpos;
+		        return n;
+		    }
+		    /* Travel up one level in the tree and try to add there */
+		    else {
+		        return tree(bit, byte, curpos->next);
+		    }
+		}
+		
+		/* If the bit is zero, add the character to a leaf node */
+		if (bit == 1) {
+		    /* Try to add leaf on left */
+		    if (curpos->left == NULL) {
+		        Node * n = node_construct(0, byte);
+		        curpos->left = n;
+		        n->next = curpos;
+		        return curpos;
+		    }
+		    /* Try to add leaf on the right */
+		    else if (curpos->right == NULL) {
+		        Node * n = node_construct(0, byte);
+		        curpos->right = n;
+		        n->next = curpos;
+		        return curpos;
+		    }
+		    /* Travel up one level in the tree and try to add there */
+		    else {
+		        return tree(bit, byte, curpos->next);
+		    }
+		}
+		 
+		 /* Should never reach this statement */
+		return NULL;
 	} 
 	
-	Node * tree(char bit, Node * curpos) {
-		/* curpos == NULL when tree is built 
-		 * byte is invalid unless bit == 1
-		 */
+	
+	
+	/* This function traverses the binary tree and prints a character in the
+	 * output file when it reaches a leaf node
+	 */
+	Node * tree2(char bit, Node * curpos) {
+		
 		 
 		/* This function will print to file */
 		
